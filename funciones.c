3edd90c9ca25afafbfaf350173c249f66d380b36
Kevin <kevinenriqueoogm@gmail.com>
2020-04-20 20:21:57 -0500

    README para la version chocolate

---
+++ funciones.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int suma(int a, int b){
+	return a+b;
+}
+
+int resta(int a, int b){
+	return a-b;
+}
--- main.c
+++ main.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+int suma(int a, int b){
+	return a+b;
+}
+
+void saludo(){
+	print("This is a print comming from a c program\n");
+}
+
 int main(){
 	
-	print("This is a print comming from a c program\n");
+
 	return 0;
 }
 
